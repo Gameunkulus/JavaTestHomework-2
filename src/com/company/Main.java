b9e04a79178ee7fb8366ef2547205414f5287a99
Gameunkulus <bugr24-8@mail.ru>
2023-05-24 18:02:54 +0300

    programm redacted 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
         int rubles = 13676;
         int miles = 20;
-        int bonuses = rubles/miles;
+        int bonuses = rubles / miles;
         System.out.println(bonuses);
     }
 }
